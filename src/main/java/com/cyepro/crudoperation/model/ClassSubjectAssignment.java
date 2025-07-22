2a602b2e86de0d56a4fe2af0d1ae3f40f60c0a20
EC2 Default User <ec2-user@ip-172-31-88-79.ec2.internal>
2025-07-22 13:44:55 +0000

    Final Commit

--- src/main/java/com/cyepro/crudoperation/model/ClassSubjectAssignment.java
+++ src/main/java/com/cyepro/crudoperation/model/ClassSubjectAssignment.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "Class_Subject_Assignments", uniqueConstraints = {
